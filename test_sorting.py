c0172f864c27d092c4419c2f4852a129fd364017
Artem Gafetinov <artem.gafetinov@gmail.com>
2018-05-12 13:04:59 +0500

    notify time

--- test_sorting.py
+++ test_sorting.py
@@ -1,9 +1,22 @@
 import sorting
+import random
 
 
 class TestSelectionSort:
     def test_simple(self):
         array = [3, 324, 2, 1, 4, 0]
+        sorted_array = array
+        sorted_array.sort()
+        assert sorting.quadratic_sort(array) == sorted_array
+        assert sorting.shell_sort(array) == sorted_array
+        assert sorting.quick_sort(array) == sorted_array
+        assert sorting.heap_sort(array) == sorted_array
+        assert sorting.merge_sort(array) == sorted_array
+
+    def test_random_array(self):
+        array = []
+        for i in range(100):
+            array.append(random.random())
         sorted_array = array
         sorted_array.sort()
         assert sorting.quadratic_sort(array) == sorted_array
