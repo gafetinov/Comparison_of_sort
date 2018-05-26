508ae23fb2fdc5ebfab028d85ca85314dbcb2852
artem <artem.gafetinov@gmail.com>
2018-05-26 20:32:00 +0500

    all sorting inplace add argument key for sorting

--- sorting.py
+++ sorting.py
@@ -1,56 +1,60 @@
 import math
 
 
-def selection_sort(array, start, end):
-    for i in range(len(array)):
+def standard_key(x):
+    return x
+
+
+def selection_sort(array, start, end, key=standard_key):
+    for i in range(start, end+1):
         index = i
         min = array[i]
-        for j in range(i, len(array)):
-            if array[j] < min:
+        for j in range(i, end+1):
+            if key(array[j]) < key(min):
                 min = array[j]
                 index = j
         array[i], array[index] = array[index], array[i]
 
 
-def shell_sort(array, start, end):
-    step = len(array) // 2
-    while step > 1:
-        i = 0
-        while i + step < len(array) - 1:
-            if array[i] > array[i+step]:
-                array[i], array[i+step] = array[i+step], array[i]
-            i += 1
+def shell_sort(array, start, end, key=standard_key):
+    step = (end-start+1)//2
+    while step >= 1:
+        for i in range(start, end-step+1):
+            j = i
+            while j >= start and key(array[j]) > key(array[j+step]):
+                array[j], array[j+step] = array[j+step], array[j]
+                j -= 1
         step //= 2
 
 
-def quick_sort(array, start, end):
+def quick_sort(array, start, end, key=standard_key):
     left = start
     right = end
     pivot = array[(left+right)//2]
     while left <= right:
-        while array[left] < pivot:
+        while key(array[left]) < key(pivot):
             left += 1
-        while array[right] > pivot:
+        while key(array[right]) > key(pivot):
             right -= 1
         if left <= right:
             array[left], array[right] = array[right], array[left]
             left += 1
             right -= 1
     if right > start:
-        quick_sort(array, start, right)
+        quick_sort(array, start, right, key)
     if end > left:
-        quick_sort(array, left, end)
+        quick_sort(array, left, end, key)
 
 
-def heap_sort(array, start, end):
+def heap_sort(array, start, end, key=standard_key):
     sl = len(array)
 
     def swap(pi, ci):
-        if array[pi] < array[ci]:
+        if key(array[pi]) < key(array[ci]):
             array[pi], array[ci] = array[ci], array[pi]
 
     def sift(pi, unsorted):
-        i_gt = lambda a, b: a if array[a] > array[b] else b
+        i_gt = lambda a, b: a if key(array[a]) > key(array[b]) else b
         while pi * 2 + 2 < unsorted:
             gtci = i_gt(pi * 2 + 1, pi * 2 + 2)
             swap(pi, gtci)
@@ -65,50 +69,54 @@ def heap_sort(array, start, end):
         sift(0, i)
 
 
-def merge_sort(array, start, end):
-    def merge(left, right):
-        lst = []
-        while left and right:
-            if left[0] < right[0]:
-                lst.append(left.pop(0))
+def merge_sort(array, start, end, key=standard_key):
+    def merge(array, start, mid, end):
+        left = array[start:mid]
+        right = array[mid:end]
+        i = 0
+        j = 0
+        k = start
+        for l in range(k, end):
+            if j >= len(right) or (i < len(left) and key(left[i]) < key(right[j])):
+                array[l] = left[i]
+                i = i + 1
             else:
-                lst.append(right.pop(0))
-        if left:
-            lst.extend(left)
-        if right:
-            lst.extend(right)
-        return lst
+                array[l] = right[j]
+                j = j + 1
 
-    length = len(array)
-    if length >= 2:
-        mid = int(length / 2)
-        array = merge(merge_sort(array[:mid]), merge_sort(array[mid:]))
-    return array
+    end += 1
+    if end - start > 1:
+        mid = (start + end) // 2
+        merge_sort(array, start, mid-1, key)
+        merge_sort(array, mid, end-1, key)
+        merge(array, start, mid, end)
 
 
-def insertion_sort(array, start, end):
+def insertion_sort(array, start, end, key=standard_key):
     for i in range(1, len(array)):
-        key = array[i]
+        elem = array[i]
         j = i - 1
-        while (j > -1) and key < array[j]:
+        while (j > -1) and key(elem) < key(array[j]):
             array[j + 1] = array[j]
             j = j - 1
-        array[j + 1] = key
+        array[j + 1] = elem
 
 
-def bin_insert_sort(array, start, end):
-    for i in range(len(array)):
-        if array[i-1] > array[i]:
-            x = array[i]
-            left = 0
-            right = i-1
-            while left <= right:
-                mid = (left+right)/2
-                if array[mid] < x:
-                    left = mid+1
-                else:
-                    right = mid-1
-            for j in range(i-1, left-1, -1):
-                array[j+1] = array[j]
-                array[left] = x
+def bin_insert_sort(array, start, end, key=standard_key):
+    def bin_search(array, key, start, end):
+        left = start-1
+        right = end-start+1
+        while left < right-1:
+            mid = (left+right)//2
+            if array[mid] < key:
+                left = mid
+            else:
+                right = mid
+        return right
+
+    for i in range(start+1, end):
+        j = i-1
+        k = bin_search(array, array[i], start, end)
+        for m in range(j, k, -1):
+            array[m], array[m+1] = array[m+1], array[m]
 # some sorting is inplace
--- test_sorting.py
+++ test_sorting.py
@@ -1,17 +1,24 @@
 import sorting
 import random
-SORTINGS = (sorting.selection_sort, sorting.quick_sort,
-            sorting.heap_sort, sorting.merge_sort, sorting.insertion_sort,
-            sorting.bin_insert_sort)
+SORTINGS = (sorting.selection_sort, sorting.quick_sort, sorting.shell_sort,
+            sorting.heap_sort, sorting.merge_sort, sorting.insertion_sort)
 
 
 class TestSelectionSort:
     def test_quick(self):
-        array = [3, 324, 2, 1, 4, 0]
+        array = [3, 324, 2, 1, 4, 0, -34]
+        sorted_array = list(array)
+        sorted_array.sort(key=lambda x: abs(x))
+        lst = list(array)
+        sorting.heap_sort(lst, 0, len(lst)-1, key=lambda x: abs(x))
+        assert lst == sorted_array
+
+    def test_bin(self):
+        array = [3, 1, 4, 0, 28]
         sorted_array = list(array)
         sorted_array.sort()
         lst = list(array)
-        sorting.quick_sort(lst, 0, len(lst)-1)
+        sorting.bin_insert_sort(lst, 0, len(lst)-1)
         assert lst == sorted_array
 
     def test_simple(self):
@@ -20,7 +27,7 @@ class TestSelectionSort:
         sorted_array.sort()
         for sorting in SORTINGS:
             lst = list(array)
-            sorting(lst)
+            sorting(lst, 0, len(lst)-1)
             assert lst == sorted_array
 
     def test_random_array(self):
