21401393a9e1a57fd6f3c585088d327fc6f00098
gafetinov <artem.gafetinov@gmail.com>
2018-05-26 13:36:16 +0500

    Update test_sorting.py

--- test_sorting.py
+++ test_sorting.py
@@ -1,26 +1,35 @@
 import sorting
 import random
+SORTINGS = (sorting.selection_sort, sorting.quick_sort,
+            sorting.heap_sort, sorting.merge_sort, sorting.insertion_sort,
+            sorting.bin_insert_sort)
 
 
 class TestSelectionSort:
+    def test_quick(self):
+        array = [3, 324, 2, 1, 4, 0]
+        sorted_array = list(array)
+        sorted_array.sort()
+        lst = list(array)
+        sorting.quick_sort(lst, 0, len(lst)-1)
+        assert lst == sorted_array
+
     def test_simple(self):
         array = [3, 324, 2, 1, 4, 0]
-        sorted_array = array
+        sorted_array = list(array)
         sorted_array.sort()
-        assert sorting.quadratic_sort(array) == sorted_array
-        assert sorting.shell_sort(array) == sorted_array
-        assert sorting.quick_sort(array) == sorted_array
-        # assert sorting.heap_sort(array) == sorted_array
-        assert sorting.merge_sort(array) == sorted_array
+        for sorting in SORTINGS:
+            lst = list(array)
+            sorting(lst)
+            assert lst == sorted_array
 
     def test_random_array(self):
         array = []
         for i in range(100):
-            array.append(random.randint(-10000, 10000))
-        sorted_array = array
+            array.append(random.random())
+        sorted_array = list(array)
         sorted_array.sort()
-        assert sorting.quadratic_sort(array) == sorted_array
-        assert sorting.shell_sort(array) == sorted_array
-        assert sorting.quick_sort(array) == sorted_array
-        # assert sorting.heap_sort(array) == sorted_array
-        assert sorting.merge_sort(array) == sorted_array
+        for sorting in SORTINGS:
+            lst = list(array)
+            sorting(lst, 0, len(lst)-1)
+            assert lst == sorted_array
